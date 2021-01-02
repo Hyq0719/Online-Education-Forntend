4985958efc333b84188050412d9de7ad2d61b966
陈刀仔 <l>
2021-01-02 21:25:35 +0800

    commit nav2

---
+++ src/views/childcpn/passage.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="grid-content bg-purple">
+        <el-row>
+            <img src="@/assets/logo.png" alt="">
+        </el-row>
+        <el-row>
+             {{label}}
+        </el-row>
+    </div>
+</template>
+
+<style>
+  .el-row {
+    margin-bottom: 20px;
+
+  }
+  .el-col {
+    border-radius: 4px;
+  }
+  .bg-purple {
+    background: #d3dce6;
+  }
+  .bg-purple-light {
+    background: #e5e9f2;
+  }
+  .grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+  }
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
+<script>
+export default {
+  name: 'classview1',
+  props: {
+    img: {
+      type: String,
+      default: require('@/assets/logo.png')
+    },
+    label: {
+      type: String,
+      default: '广告位'
+    }
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
