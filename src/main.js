8a3536fffff9721b3bc214d74b6ba927459c4ae5
Riosted <44918545+Riosted@users.noreply.github.com>
2021-01-02 18:58:16 +0800

    1/2

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
+/* jshint esversion：6 */
+
 import Vue from 'vue'
 import App from './App.vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
