2de05ca0eaed2a068ea5fe52baebe29238f68562
xiyiabusang <75316554+xiyiabusang@users.noreply.github.com>
2021-01-02 20:43:48 +0800

    Add files via upload

---
+++ src/components/myfooter.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="footer">
+        <p>大学生在线教育平台</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"footer",
+}
+</script>
+
+<style>
+.footer{
+    background: black;
+    height: 60px;
+}
+.footer p{
+    color: white;
+    font-size: 14px;
+    margin: 23px;
+    display: inline-block;
+}
+</style>
---
+++ src/components/myheader.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <el-menu
+      :default-active="activeIndex"
+      class="el-menu-demo"
+      mode="horizontal"
+      @select="handleSelect"
+    >
+      <div class="myheader">
+        <img src="../../build/logo.png" alt="加载失败" style="height:60px; width:60px;"/>
+      </div>
+      <div class="myheader-search">
+        <el-input placeholder="请输入内容" v-model="input" clearable>
+        </el-input>
+      </div>
+      <div class="myheader">
+        <img src="../../pictures/search.jpg" alt="加载失败" style="height:40px; width:40px; margin:10px 0px;"/>
+      </div>
+
+      <el-menu-item index="1"
+        ><router-link to="/main/login">登录</router-link></el-menu-item
+      >
+      <el-submenu index="2">
+        <template slot="title">分类</template>
+        <el-menu-item index="2-1">选项1</el-menu-item>
+        <el-menu-item index="2-2">选项2</el-menu-item>
+        <el-menu-item index="2-3">选项3</el-menu-item>
+        <el-submenu index="2-4">
+          <template slot="title">选项4</template>
+          <el-menu-item index="2-4-1">选项1</el-menu-item>
+          <el-menu-item index="2-4-2">选项2</el-menu-item>
+          <el-menu-item index="2-4-3">选项3</el-menu-item>
+        </el-submenu>
+      </el-submenu>
+      <el-menu-item index="3">消息中心</el-menu-item>
+      <el-menu-item index="4"
+        ><router-link to="/main">直播</router-link></el-menu-item
+      >
+      <el-menu-item index="5"
+        ><router-link to="/main">首页</router-link></el-menu-item
+      >
+    </el-menu>
+  </div>
+</template>
+
+<script>
+export default {
+  name:"header",
+  data() {
+    return {
+      activeIndex: "1",
+      activeIndex2: "1",
+      input:'',
+    };
+  },
+  methods: {
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath);
+    },
+  },
+};
+</script>
+
+<style>
+.myheader {
+  float: left;
+  height: 60px;
+  width: 60px;
+}
+.myheader-search{
+    float:left;
+    width:200px;
+    margin:10px 0px 10px 50px;
+}
+
+.el-menu--horizontal > .el-menu-item {
+  float: right;
+}
+.el-menu--horizontal > .el-submenu {
+  float: right;
+}
+</style>
