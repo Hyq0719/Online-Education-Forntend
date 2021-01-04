2bcf9dae5f7a42da29e592e71cc17a6888614243
xiyiabusang <75316554+xiyiabusang@users.noreply.github.com>
2021-01-04 12:51:01 +0800

    删除组件中APP的CSS

--- src/components/Nav2.vue
+++ src/components/Nav2.vue
@@ -81,15 +81,6 @@
   padding: 10px 0;
   background-color: #f9fafc;
 }
-
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
 
 <script>
--- src/components/childcpn/adview.vue
+++ src/components/childcpn/adview.vue
@@ -31,15 +31,6 @@
   border-radius: 4px;
   min-height: 36px;
 }
-
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
 
 <script>
