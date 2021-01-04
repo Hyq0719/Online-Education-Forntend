c5ad2d64bb1f7f2933916e964c715179f64b8db1
Riosted <44918545+Riosted@users.noreply.github.com>
2021-01-04 20:45:58 +0800

    在Course.vue实现了视频点播功能 下载了视频播放插件 vue-core-video-player

--- src/main.js
+++ src/main.js
@@ -4,10 +4,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import VueCoreVideoPlayer from 'vue-core-video-player'
 import router from './router'
 
-Vue.use(router)
+
 Vue.use(ElementUI)
+Vue.use(VueCoreVideoPlayer, {
+    lang: 'zh-CN'
+})
+Vue.use(router)
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/Courses.vue
+++ src/views/Courses.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
     <Header></Header>
+    <vue-core-video-player class="myVedio"
+                           src="https://shu-online-edu.oss-cn-shanghai.aliyuncs.com/video/0/2.1.1.2%20%E6%A0%87%E5%87%86%E5%8C%96%E5%B7%A5%E4%BD%9C%E5%8F%8A%E7%9B%B8%E5%85%B3%E7%BB%84%E7%BB%87%28Av70228743%2CP2%29.mp4
+">
+    </vue-core-video-player>
   </div>
 </template>
 
@@ -16,5 +20,11 @@ export default {
 </script>
 
 <style scoped>
+
+.myVedio {
+  height: 100%;
+  width: 100%;
+  margin: 0 auto;
+}
 
 </style>
