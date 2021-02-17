563dc153e1334890d13f04a943e40d6acb3a8e60
xiyiabusang <75316554+xiyiabusang@users.noreply.github.com>
2021-02-17 18:13:51 +0800

    压缩Js

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,14 @@
 module.exports = {
-    lintOnSave: false
+    lintOnSave: false,
+    configureWebpack: config => {
+        if (process.env.NODE_ENV === "production") {
+            config.externals = { // 不会被打包的库
+                'vue': 'Vue',
+                'vue-router': 'VueRouter',
+                'element-ui': 'element-ui',
+            }
+        }
+    },
+    parallel: require('os').cpus().length > 1,
 }
+
